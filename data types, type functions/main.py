fe8946193db69255dc087b1714531f033f54801b
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-01-28 00:46:58 +0500

    first commit

---
+++ data types, type functions/main.py
@@ -0,0 +1,20 @@
+# type
+
+# int
+num = 100
+
+# str
+string = "Name"
+
+# float
+dr = 1.0
+
+# bool
+boolean = True
+
+# type(data)
+
+# input("Text")
+console = input("Name: ")
+
+# int('') => parseInt
---
+++ math operations/main.py
@@ -0,0 +1,9 @@
+# + * / - %
+
+# // for dividing int and math floor it 5 // 2 = 2
+
+# ** for math pow
+
+# abs(num) for absolute number
+
+# int() float() str() bool() - only 0 false
